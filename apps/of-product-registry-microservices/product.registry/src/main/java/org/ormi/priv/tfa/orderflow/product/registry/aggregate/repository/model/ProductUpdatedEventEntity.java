79fa313b424e9e4fec6056fa3cd825f89d025aa8
AnthonyGardelle <a.gardelle18@gmail.com>
2025-01-19 19:27:02 +0100

    refactor: remplacez les classes Payload par des enregistrements pour les entités d'événement ProductRegistered, ProductRemoved et ProductUpdated

--- apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductUpdatedEventEntity.java
+++ apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductUpdatedEventEntity.java
@@ -1,34 +1,21 @@
 package org.ormi.priv.tfa.orderflow.product.registry.aggregate.repository.model;
 
 /**
-     * Event entity for a product which is updated
-     */
+ * Event entity for a product which is updated
+ */
 public class ProductUpdatedEventEntity extends ProductRegistryEventEntity {
   static final String EVENT_TYPE = "ProductUpdated";
 
   /**
    * Payload for the event.
    */
-  public static class Payload {
-    /**
-     * The id of the product.
-     */
-    public String productId;
-    /**
-     * The name of the product.
-     */
-    public String name;
-    /**
-     * The description of the product.
-     */
-    public String productDescription;
+  public static record Payload(String productId, String name, String productDescription) {
   }
 
   /**
    * The payload for the event.
    */
   public Payload payload;
-
 
   @Override
   public String getEventType() {
